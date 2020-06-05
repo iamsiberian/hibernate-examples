6886b4467b5ff6898b99f702b59b13621093f57f
Minyukhin Ilya <minyukhin.ilya@gmail.com>
2020-06-05 17:58:24 +0600

    add one-to-many:bidirectional

---
+++ hibernate-examples-core/src/main/java/com/lineate/api/core/domain/onetomany/bidirectional/Price.java
@@ -0,0 +1,38 @@
+package com.lineate.api.core.domain.onetomany.bidirectional;
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "prices")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Price {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    protected Long id;
+
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    protected Product product;
+
+    @NotNull
+    protected BigDecimal amount;
+}
---
+++ hibernate-examples-core/src/main/java/com/lineate/api/core/repositories/onetomany/bidirectional/PriceRepository.java
@@ -0,0 +1,17 @@
+package com.lineate.api.core.repositories.onetomany.bidirectional;
+
+import com.lineate.api.core.domain.onetomany.bidirectional.Price;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Collection;
+
+public interface PriceRepository extends JpaRepository<Price, Long> {
+    @Query(
+        "select p " +
+            "from Price p " +
+            "where p.product.id = :productId"
+    )
+    Collection<Price> getPricesByProductId(@Param("productId") Long productId);
+}
